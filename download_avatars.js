35fabe68f3e25662cd3532fa365baa0e7c8367cc
Alexandra Sia <alexandrasia@gmail.com>
2017-05-09 03:18:11 +0000

    Commit of final version of GitHub Avatar Downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
+var https = require('https');
 var request = require('request');
+var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = "alexandrasia";
+var GITHUB_TOKEN = "34e385afa92200d96841cdd8f866ef962a008d07";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'alexandrasia'
+    }
+  };
+  request(options, cb);
+}
+
+function downloadImageByURL (url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
+}
+
+getRepoContributors(process.argv[2], process.argv[3], function(err, result, body) {
+  var parsedResults = JSON.parse(body);
+  for (var i = 0; i < parsedResults.length; i++) {
+    var avatarUrl = parsedResults[i]["avatar_url"];
+    downloadImageByURL(avatarUrl, `avatars/${i}.jpg`);
+  }
+});
