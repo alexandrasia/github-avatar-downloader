6534b49b39aedaa587465d73592e4dd31ea2b31b
Alexandra Sia <alexandrasia@gmail.com>
2017-05-10 01:08:59 +0000

    Final version with added error message

--- download_avatars.js
+++ download_avatars.js
@@ -6,6 +6,10 @@ var GITHUB_USER = "alexandrasia";
 var GITHUB_TOKEN = "34e385afa92200d96841cdd8f866ef962a008d07";
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  if (!repoOwner | !repoName) {
+    console.log("Please specify a valid Repo Owner and Repo Name!");
+    return;
+  }
   var options = {
     url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
